1f4f391aface00d3fa5f723dc716a600b2cb9773
Rashad Dweik <rashaddweik20@gmail.com>
2025-08-14 12:31:24 +0300

    removed router.php

--- router.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (file_exists(__DIR__ . '/public' . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
-    return false; // serve the requested resource as-is
-} else {
-    require __DIR__ . '/public/index.php';
-}
